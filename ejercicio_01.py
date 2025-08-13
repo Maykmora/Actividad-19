9b69a4f12787c379f5cff55404ebe7936f1bab8a
Maykmora <maykmora12@gmail.com>
2025-08-12 19:12:51 -0600

    Commit 1 Menú

---
+++ ejercicio_01.py
@@ -0,0 +1,8 @@
+def menu():
+    print("1.Registrar galleta básica")
+    print("2.Registrar galleta con chispas")
+    print("3.Registrar galleta rellena")
+    print("4.Listar galletas")
+    print("5.Buscar por nombre")
+    print("6.Eliminar por nombre")
+    print("7.Salir ")
