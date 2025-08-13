9e5b7a56fda4885ae39209dbfeb38d3fcfc47419
Maykmora <maykmora12@gmail.com>
2025-08-13 16:16:02 -0600

    Commit 2 Menú

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -6,3 +6,23 @@ def menu():
     print("5.Buscar por nombre")
     print("6.Eliminar por nombre")
     print("7.Salir ")
+
+while True:
+    option=input("Seleccione una opción del menú (1-7):")
+    match option:
+        case "1":
+            print()
+        case "2":
+            print()
+        case "3":
+            print()
+        case "4":
+            print()
+        case "5":
+            print()
+        case "6":
+            print()
+        case "7":
+            print()
+        case _:
+        print()
